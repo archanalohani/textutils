5d5f865d7ee8db82b98cf9c218b481d34c08e791
archanalohani <archanalohani42@gmail.com>
2021-01-09 20:47:51 +0530

    Modify view for new index file and other fixes

--- textutils/views.py
+++ textutils/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index2.html')
 
 def ex1(request):
     sites=[''' <h1>for Entertainment </h1><a href=https://www.youtube.com >youtube video</a>'''
@@ -18,12 +18,13 @@ def ex1(request):
 def analyze(request):
 #get the first text
     djtext = request.GET.get('text', 'default')
-
+  
     #check checkbox values
 
     removepunc=request.GET.get('removepunc','off')
     fullcaps = request.GET.get('fullcaps', 'off')
     newlineremover=request.GET.get('newlineremover','off')
+    extraspaceremover=request.GET.get('extraspaceremover','off')
 
 #check which checkbox is on
 
@@ -34,7 +35,7 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, ' .html', params)
+        return render(request, 'analyze.html', params)
     elif(fullcaps=="on"):
         analyzed=""
         for char in djtext:
@@ -42,13 +43,14 @@ def analyze(request):
         params={'purpose':'changed to uppercase','analyzed_text':analyzed}
         return render(request,'analyze.html',params)
 
-    elif(newlineremover=="on"):
+   
+    elif(extraspaceremover=="on"):
         analyzed=""
-        for char in djtext:
-            if char!="\n":
-               analyzed=analyzed+char
-               params={'purpose':'RemoveNewLine','analyzed_text':analyzed}
+        for index,char in enumerate(djtext):
+            if not djtext[index]==" " and djtext[index+1]:
+              analyzed=analyzed+char
+        params={'purpose':'extra space remover','analyzed_text':analyzed}
         return render(request,'analyze.html',params)
 
     else:
-        return HttpResponse('Error')
+        return HttpResponse('Error') 
